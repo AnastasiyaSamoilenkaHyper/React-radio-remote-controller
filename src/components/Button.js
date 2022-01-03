5a9175024b02a586c4aa38f208bea9835d07b32c
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-01-03 19:17:51 +0100

    start and stop recording works

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,24 +5,30 @@ import React, { useState, useEffect } from "react";
 function Button() {
   const SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition; 
-    const recognition = new SpeechRecognition();
+  const recognition = new SpeechRecognition();
+  
+  let recordingTrue = false;
   
   if (SpeechRecognition) {
     console.log("Your Browser supports speech Recognition");
     recognition.continuous = true;
     recognition.lang = "en-US";
     recognition.start();
+    recordingTrue = true;
   }
 
   const [microphoneActivator, setmicrophoneActivator] = useState(true);
   
   function micBtnClick() {
     console.log("micBtn clicked");
-    if (microphoneActivator) {
+
+    if (!recordingTrue) {
       setmicrophoneActivator(false);
-      recognition.start(); 
+      recognition.start();
+      recordingTrue = true;
     } else {
       recognition.stop();
+      recordingTrue = false;
     }
   }
 
