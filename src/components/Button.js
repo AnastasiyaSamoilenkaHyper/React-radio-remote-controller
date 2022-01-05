5223b016be72c785e74cd302ec56c45669dc09f6
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-01-05 23:32:24 +0100

    button commands fires 3 times

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,69 @@
 import microphone from "./assets/microphone-solid.svg";
 import React, { useState, useEffect } from "react";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+let speachToggle = false;
 
 function Button() {
-  const { transcript } = useSpeechRecognition();
+  
 
-  let speachToggle = false;
   function speachRecognitionToggle() {
     if (!speachToggle) {
+      console.log("hii")
       speachToggle = true;
-      SpeechRecognition.startListening();
+      SpeechRecognition.startListening({ continuous: true, language: "en-Us" });
     } else {
-      speachToggle = false;
+      
       SpeechRecognition.stopListening();
+      console.log("Speach");
+      speachToggle = false;
+      // SpeechRecognition.startListening({ continuous: false, language: "en-Us" });
     }
   }
 
+  useEffect(() => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Ups, your browser is not supported!");
+    }
+  }, []);
+
+  const commands = [
+    {
+      command: "Hello",
+      callback: () => alert("You just said hi!!"),
+      matchInterim: true,
+    },
+    {
+      command: "stop",
+      callback: () => alert("You just said stop!!"),
+      matchInterim: true,
+    },
+  ];
+
+  useEffect(() => {
+    console.log("Useeefect");
+  }, []);
+
+  const { transcript } = useSpeechRecognition({ commands });
+
   return (
     <div className="Button">
-      <button onClick={speachRecognitionToggle}> Speak
+      <button onClick={speachRecognitionToggle}>
+        {" "}
+        Speak
         <img src={microphone} className="microphoneOn" />
       </button>
-      <form action = "https://www.google.com/search" method = "get" target = "_blank" className="search-form">
-        <input className="searchInput" name="q" type="text" placeholder="Speak please..." autoComplete="off" autoFocus={true} />
 
+      <form
+        action="https://www.google.com/search"
+        method="get"
+        target="_blank"
+        className="search-form"
+      >
+        {/* <input className="searchInput" name="q" type="text" placeholder="Speak please..." autoComplete="off" autoFocus={true}  /> */}
+        <p>{transcript ? transcript : "Start listening for transcript"}</p>
       </form>
     </div>
   );
