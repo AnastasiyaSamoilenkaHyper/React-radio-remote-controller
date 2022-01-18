cfb1888d79c5f946eefeaf09bcd878d8ef0854af
AnastasiyaSam <anastasiya.samoilenka@hyperisland.se>
2022-01-18 21:37:19 +0100

    changed text

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dogs() {
   return (
     <div>
-      <p style= {{color: "white"}}>Here you can see photos of the dog</p>
+      <p style= {{color: "white"}}>Here you can see a photo of the dog</p>
 
       <img src="https://images.unsplash.com/photo-1642365585811-17521651de66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" rel="photo of a dog" style={{ maxHeight: "600px"}}></img>
 
